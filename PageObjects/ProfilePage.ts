d0f5b31ac52a9507dea6ad7e2da8c5d5853940e0
nalabi <bongimusa.nala@gmail.com>
2024-07-24 16:43:07 +0200

    Test 458978

--- PageObjects/ProfilePage.ts
+++ PageObjects/ProfilePage.ts
@@ -2,7 +2,9 @@ import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ProfilePage extends BasePage{
-    
+readonly letStartBtn: Locator
+readonly meOption: Locator
+readonly continueBtn: Locator
 readonly emailAddressText: Locator
 readonly confirmEmailAddressText: Locator
 readonly UsernameText: Locator
@@ -13,6 +15,8 @@ readonly PasswordText: Locator
     constructor(page: Page) {
         super(page);
         this.emailAddressText= page.locator("//label[@class='MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-shrink'][contains(.,'Email address')]");
-
+        this.letStartBtn = page.locator("//button[contains(@class,'MuiButtonBase-root MuiButton-root MuiButton-text Button__BaseButton-sc-o2ehon-1 bjVRlj MuiButton-disableElevation MuiButton-fullWidth')]");
+        this.meOption = page.locator("//h5[@class='MuiTypography-root Typography__StyledTypography-sc-1cltmdr-0 fvBUuZ MuiTypography-body1'][contains(.,'Me')]");
+        this.continueBtn = page.locator("//span[@class='MuiButton-label'][contains(.,'Continue')]");
     }
 }
--- tests/CreateProfile.spec.ts
+++ tests/CreateProfile.spec.ts
@@ -7,6 +7,13 @@ test.describe.parallel('Pofile Creation', () =>{
     test.beforeEach(async({page}) =>{
         profilePage = new ProfilePage(page)
         await page.goto('https://my.account.bestinvest.co.uk/onboarding/create-profile')
+        await profilePage.letStartBtn.click()
+        await profilePage.meOption.click()
+        await profilePage.continueBtn.click()
+
+    })
+    test('Lets Start button', async({page}) =>{
+        await expect
     })
     test('Email Text', async ({page})=>{
            await expect(profilePage.emailAddressText).toBeVisible(); 
